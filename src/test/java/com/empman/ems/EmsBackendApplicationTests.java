ff149f7f726c1253d109c70f787158c26c6c56ec
= <=>
2025-11-03 21:45:51 +0530

    Initial Commit

---
+++ src/test/java/com/empman/ems/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empman.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
