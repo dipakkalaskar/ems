c7408f69285fd9b8fa00c3ec90565393805f4f7d
Dipak Kalaskar <dipak.k@pixel6.co>
2025-11-04 15:56:51 +0530

    Added CrossOrigin Annotation

--- src/main/java/com/empman/ems/controller/EmployeeController.java
+++ src/main/java/com/empman/ems/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ import com.empman.ems.dto.EmployeeDto;
 import com.empman.ems.service.EmployeeService;
 
 import lombok.AllArgsConstructor;
-
+@CrossOrigin("*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
